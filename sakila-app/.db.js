b391de2cda8b2fb8dd5b901f2b5bb6a66c084057
Karim  Alkichouhi <59794989+karim076@users.noreply.github.com>
2025-09-03 00:31:35 +0100

    Switch from mysql2/promise pool to mysql2 connection
    Replaces the use of mysql2's promise-based connection pool with a standard mysql2 connection in .db.js. Updates module export accordingly for compatibility with code expecting a single connection instance.

--- sakila-app/.db.js
+++ sakila-app/.db.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'localhost', // (Default)
-  user: 'root',     // pas aan naar jouw MySQL user (Default)
-  password: '', // pas aan naar jouw MySQL wachtwoord (wachtwoord die je heb gemaakt bij MySQL installatie)
-  database: 'sakila' // pas aan naar je eigen database naam. (het moet wel matchen)
+const connection = mysql.createConnection({
+  host: 'localhost',  // (Default)
+  user: 'root',       // pas aan naar jouw MySQL user (Default)
+  password: '',       // pas aan naar jouw MySQL wachtwoord (wachtwoord die je heb gemaakt bij MySQL installatie)
+  database: 'sakila'  // pas aan naar je eigen database naam. (het moet wel matchen)
 });
-// Verwijder de . achter de db om het werkend te krijgen.
-module.exports = pool;
+// Verwijder de . achter de db om het werkend te krijgen. (!BELANGERIJK!)
+
+module.exports = connection;
